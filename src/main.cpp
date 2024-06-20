265fa3a3706b3b85592bcecc6ead82affd26066a
m-ghozi <ghozi286@gmail.com>
2024-06-20 17:49:51 +0700

    test gps

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <Arduino.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
-void setup() {
+HardwareSerial GPSSerial(1);
 
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+void setup(){
+  Serial.begin(9600);
+  GPSSerial.begin(9600, SERIAL_8N1, 16, 17); // The serial connection to the GPS device
+  Serial.print("tes");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (GPSSerial.available() > 0){
+    gps.encode(GPSSerial.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);
+    }
+    else {
+      Serial.println("INVALID");
+    }
+    delay(500);
+  }
 }
