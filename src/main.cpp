309d760d24965ebd4d0134fdca95f205ae6b4109
m-ghozi <ghozi286@gmail.com>
2024-06-21 15:08:38 +0700

    setup oled

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,50 @@
 #include <Arduino.h>
 #include <TinyGPS++.h>
 #include <HardwareSerial.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
 
 HardwareSerial GPSSerial(1);
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
+// Setup Oled
+#define i2c_Address 0x3C // I2C address of the OLED display
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup(){
   Serial.begin(9600);
   GPSSerial.begin(9600, SERIAL_8N1, 16, 17); // The serial connection to the GPS device
-  Serial.print("tes");
+  Serial.print("GAMe : GPS Angkot Manajemen");
+  // Begin Oled
+  display.begin(i2c_Address, true);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SH110X_WHITE);
+  display.setCursor(0, 0);
+  display.println("GAMe");
+  display.display();
 }
 
 void loop(){
+  // Show Data To Oled
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(40, 0);
+  display.print("GAMe");
+  display.setTextSize(1);
+  display.setCursor(0, 25);
+  display.print("Jenis : Suzuki Carry");
+  display.setCursor(0, 40);
+  display.print("Plat  : BA 2592 MY");
+  display.setCursor(0, 55);
+  display.print("Warna : Biru");
+  display.display();
+
   // This sketch displays information every time a new sentence is correctly encoded.
   while (GPSSerial.available() > 0){
     gps.encode(GPSSerial.read());
@@ -23,9 +54,5 @@ void loop(){
       Serial.print(" Longitude= "); 
       Serial.println(gps.location.lng(), 6);
     }
-    else {
-      Serial.println("INVALID");
-    }
-    delay(500);
   }
 }
